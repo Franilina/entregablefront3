78418e03aa2435f4a6b0437119fce33193e7464c
Pauli <ppalma.paulina@gmail.com>
2024-02-28 22:03:32 -0300

    actualizar

---
+++ src/cards.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Este componente debería recibir por props y mostrar en pantalla la información
+// que envía el usuario
+function Card() {
+  return (
+    <div>
+      <h2>Esto es un componente</h2>
+    </div>
+  );
+}
+
+export default Card;
